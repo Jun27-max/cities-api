ac6e5289ccd4cdc004790a03a07ed1969efcda05
Junior <junior.uniseb8@gmail.com>
2021-12-26 11:40:33 -0300

    Início do Projeto API-Cities

---
+++ src/test/java/me/dio/citiesapi/citiesapi/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.citiesapi.citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
